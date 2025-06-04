7543c41754e2b2bd114e8d1d6016e7f2d09dc579
BoD <BoD@JRAF.org>
2025-06-04 11:41:06 +0200

    Make ApolloCacheCompilerPlugin again

--- normalized-cache-apollo-compiler-plugin/src/main/kotlin/com/apollographql/cache/apollocompilerplugin/ApolloCacheCompilerPlugin.kt
+++ normalized-cache-apollo-compiler-plugin/src/main/kotlin/com/apollographql/cache/apollocompilerplugin/ApolloCacheCompilerPlugin.kt
@@ -1,14 +1,17 @@
 @file:OptIn(ApolloExperimental::class)
 
-package com.apollographql.cache.apollocompilerplugin.internal
+package com.apollographql.cache.apollocompilerplugin
 
 import com.apollographql.apollo.annotations.ApolloExperimental
 import com.apollographql.apollo.ast.ForeignSchema
 import com.apollographql.apollo.compiler.ApolloCompilerPlugin
 import com.apollographql.apollo.compiler.ApolloCompilerPluginEnvironment
 import com.apollographql.apollo.compiler.ApolloCompilerRegistry
+import com.apollographql.cache.apollocompilerplugin.internal.AddKeyFieldsExecutableDocumentTransform
+import com.apollographql.cache.apollocompilerplugin.internal.CacheSchemaCodeGenerator
+import com.apollographql.cache.apollocompilerplugin.internal.cacheGQLDefinitions
 
-internal class ApolloCacheCompilerPlugin : ApolloCompilerPlugin {
+class ApolloCacheCompilerPlugin : ApolloCompilerPlugin {
   override fun beforeCompilationStep(
       environment: ApolloCompilerPluginEnvironment,
       registry: ApolloCompilerRegistry,
