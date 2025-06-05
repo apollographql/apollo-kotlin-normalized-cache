a266c16e23fd62e8394edeb4af98e2ec120f93dd
BoD <BoD@JRAF.org>
2025-06-05 15:14:58 +0200

    Keep mavenLocal()

--- gradle/repositories.gradle.kts
+++ gradle/repositories.gradle.kts
@@ -1,6 +1,8 @@
 listOf(pluginManagement.repositories, dependencyResolutionManagement.repositories).forEach {
   it.apply {
+//    mavenLocal()
 //    maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+//    maven("https://storage.googleapis.com/apollo-previews/m2/")
     mavenCentral()
     google()
     gradlePluginPortal()
