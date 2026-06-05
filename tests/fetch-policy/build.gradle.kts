9b42afd836a14b3723a5c1dfa7ae8d57d37b93a2
Benoit 'BoD' Lubek <BoD@JRAF.org>
2026-06-05 12:41:11 +0200

    CacheFirstFetchPolicy: go to the network after a cache miss only (#351)
    * Add refetchPolicyCacheFirstWithWriteToCacheAsync test
    
    * Use SQL cache
    
    * Go to the network if there's a cache miss only - not if there are other exceptions.

---
+++ tests/fetch-policy/build.gradle.kts
@@ -0,0 +1,50 @@
+import com.apollographql.apollo.annotations.ApolloExperimental
+
+plugins {
+  alias(libs.plugins.kotlin.multiplatform)
+  id("com.apollographql.apollo")
+}
+
+kotlin {
+  configureKmp(
+      withJs = setOf(JsAndWasmEnvironment.Node),
+      withWasm = emptySet(),
+      withAndroid = false,
+      withApple = AppleTargets.Host,
+  )
+
+  sourceSets {
+    getByName("commonMain") {
+      dependencies {
+        implementation(libs.apollo.runtime)
+        implementation("com.apollographql.cache:normalized-cache")
+      }
+    }
+
+    getByName("commonTest") {
+      dependencies {
+        implementation("com.apollographql.cache:test-utils")
+        implementation(libs.apollo.mockserver)
+        implementation(libs.kotlin.test)
+        implementation(libs.turbine)
+      }
+    }
+
+    getByName("jvmTest") {
+      dependencies {
+        implementation(libs.slf4j.nop)
+      }
+    }
+  }
+}
+
+apollo {
+  service("service") {
+    packageName.set("test")
+
+    @OptIn(ApolloExperimental::class)
+    plugin("com.apollographql.cache:normalized-cache-apollo-compiler-plugin") {
+      argument("com.apollographql.cache.packageName", packageName.get())
+    }
+  }
+}
---
+++ tests/fetch-policy/src/commonTest/kotlin/test/FetchPolicyTest.kt
@@ -0,0 +1,130 @@
+package test
+
+import app.cash.turbine.test
+import app.cash.turbine.withTurbineTimeout
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.api.Error
+import com.apollographql.apollo.exception.ApolloGraphQLException
+import com.apollographql.apollo.exception.CacheMissException
+import com.apollographql.cache.normalized.FetchPolicy.CacheFirst
+import com.apollographql.cache.normalized.api.CacheHeaders
+import com.apollographql.cache.normalized.api.NormalizedCache
+import com.apollographql.cache.normalized.api.NormalizedCacheFactory
+import com.apollographql.cache.normalized.api.Record
+import com.apollographql.cache.normalized.api.RecordMerger
+import com.apollographql.cache.normalized.isFromCache
+import com.apollographql.cache.normalized.memory.MemoryCacheFactory
+import com.apollographql.cache.normalized.refetchPolicy
+import com.apollographql.cache.normalized.testing.assertErrorsEquals
+import com.apollographql.cache.normalized.testing.runTest
+import com.apollographql.cache.normalized.watch
+import com.apollographql.mockserver.MockRequestBase
+import com.apollographql.mockserver.MockResponse
+import com.apollographql.mockserver.MockServer
+import com.apollographql.mockserver.MockServerHandler
+import kotlinx.coroutines.delay
+import okio.use
+import test.cache.Cache.cache
+import kotlin.random.Random
+import kotlin.test.Test
+import kotlin.test.assertFails
+import kotlin.test.assertFalse
+import kotlin.test.assertIs
+import kotlin.test.assertTrue
+import kotlin.time.Duration.Companion.milliseconds
+
+class FetchPolicyTest {
+  @Test
+  fun writeToCacheAsyncWithRefetchPolicyCacheFirstAndServerErrors() = runTest {
+    MockServer.Builder().handler(
+        object : MockServerHandler {
+          override fun handle(request: MockRequestBase): MockResponse {
+            return MockResponse.Builder()
+                .body(
+                    // language=JSON
+                    """
+                    {
+                      "errors": [
+                        {
+                          "message": "Can't compute lastName",
+                          "path": [
+                            "me",
+                            "lastName"
+                          ]
+                        }
+                      ],
+                      "data": {
+                        "me": {
+                          "__typename": "User",
+                          "id": "1",
+                          "firstName": "${Random.nextInt()}",
+                          "lastName": null
+                        }
+                      }
+                    }
+                    """.trimIndent()
+                )
+                .build()
+          }
+        }
+    ).build().use { mockServer ->
+      ApolloClient.Builder()
+          .serverUrl(mockServer.url())
+          .cache(AsyncCacheFactory(), writeToCacheAsynchronously = true)
+          .build()
+          .use { apolloClient ->
+            apolloClient.query(MeQuery())
+                .refetchPolicy(CacheFirst)
+                .watch()
+                .test {
+                  // 1. response from the cache (cache miss)
+                  val cacheResponse1 = awaitItem()
+                  assertTrue(cacheResponse1.isFromCache)
+                  assertIs<CacheMissException>(cacheResponse1.exception)
+
+                  // 2. response from the network, with the error
+                  val networkResponse1 = awaitItem()
+                  assertFalse(networkResponse1.isFromCache)
+                  assertErrorsEquals(
+                      listOf(Error.Builder("Can't compute lastName").path(listOf("me", "lastName")).build()),
+                      networkResponse1.errors
+                  )
+
+                  // 3. with writeToCacheAsynchronously, when the network response is written to the cache, the watcher gets notified with the cache response
+                  val cacheResponse2 = awaitItem()
+                  assertTrue(cacheResponse2.isFromCache)
+                  // GraphQL error is surfaced as an exception by default (serverErrorsAsException is true)
+                  assertIs<ApolloGraphQLException>(cacheResponse2.exception)
+
+                  // That wasn't a cache miss: expect no more emissions
+                  withTurbineTimeout(200.milliseconds) {
+                    assertFails { awaitItem() }
+                  }
+
+                  cancelAndIgnoreRemainingEvents()
+                }
+          }
+    }
+  }
+}
+
+/**
+ * A cache that simulates slow writes.
+ * This removes flakiness by ensuring that when using `writeToCacheAsynchronously = true`, there is enough time to start observing the
+ * cache, before writing happens.
+ */
+private fun AsyncCacheFactory(): NormalizedCacheFactory = object : NormalizedCacheFactory() {
+  override fun create(): NormalizedCache {
+    val wrapped = MemoryCacheFactory().create()
+    return object : NormalizedCache by wrapped {
+      override suspend fun merge(
+          records: Collection<Record>,
+          cacheHeaders: CacheHeaders,
+          recordMerger: RecordMerger,
+      ): Set<String> {
+        delay(100.milliseconds)
+        return wrapped.merge(records, cacheHeaders, recordMerger)
+      }
+    }
+  }
+}
